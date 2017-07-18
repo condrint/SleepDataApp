80282ad3bb2e2f74354f15a7f628f516f9e1d53f
Trenton Condrin <condrint@gmail.com>
2017-07-18 15:01:20 -0700

    Add files via upload

--- java/comcondrint/github/sleepdata/MainActivity.java
+++ java/comcondrint/github/sleepdata/MainActivity.java
@@ -1,48 +1,53 @@
 package comcondrint.github.sleepdata;
 
-import android.content.Context;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.view.View.OnClickListener;
 import android.widget.EditText;
 import java.text.*;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
-    SharedPreferences SleepData = this.getPreferences(Context.MODE_PRIVATE);
-    SharedPreferences.Editor dataEditor = SleepData.edit();
 
+    SharedPreferences pref;
+    SharedPreferences.Editor dataEditor;
+
+    EditText RateText;
+    EditText WakeText;
+    EditText SleepText;
+
+    AlertDialog saveAlert;
+    AlertDialog formatAlert;
 
     public float[] extractData() {
 
         //Get rating 1-5 to use as value for storage
-        EditText RateText = (EditText)findViewById(R.id.RateText);
         float Rating = Float.parseFloat(RateText.getText().toString());
 
         //Extract difference between sleep/wake time to use as key for storage
-        EditText WakeText = (EditText)findViewById(R.id.WakeText);
-        EditText SleepText = (EditText)findViewById(R.id.SleepText);
         String WakeTime = WakeText.getText().toString();
         String SleepTime = SleepText.getText().toString();
 
         SimpleDateFormat format = new SimpleDateFormat("HH:mm");
-        float[] A = new float [2];
+        float[] pair = new float [2];
         try {
             Date date1 = format.parse(WakeTime);
             Date date2 = format.parse(SleepTime);
-            float difference = (date2.getTime() - date1.getTime()) * 3600000;
-            A[0] = difference;
-            A[1] = Rating;
+            float difference = Math.abs((date2.getTime() - date1.getTime())) * 3600000;
+            pair[0] = difference;
+            pair[1] = Rating;
 
         } catch (ParseException e){
-
+            ; //should never get here because I check the format before calling extractdata()
         };
-        return A;
+        return pair;
     }
 
 
@@ -50,6 +55,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+        dataEditor = pref.edit();
+
+        SleepText = (EditText)findViewById(R.id.SleepText);
+        WakeText = (EditText)findViewById(R.id.WakeText);
+        RateText = (EditText)findViewById(R.id.RateText);
+
+        saveAlert = new AlertDialog.Builder(MainActivity.this).create();
+        saveAlert.setMessage("Data saved");
+
+        formatAlert = new AlertDialog.Builder(MainActivity.this).create();
+        formatAlert.setMessage("Wrong input format");
     }
 
     public void goToRatings(View view)
@@ -67,10 +85,46 @@ public class MainActivity extends AppCompatActivity {
     public void SaveData(View view)
     {
         //amt of sleep is key, rating of ease of waking is value
-        float[] pair = extractData();
-        String amtSleep = Float.toString(pair[0]);
-        dataEditor.putInt(amtSleep , Math.round(pair[1]));
-        dataEditor.apply();
+        if (checkFormat(view)) {
+            float[] pair = extractData();
+            String amtSleep = Float.toString(pair[0]);
+            dataEditor.putInt(amtSleep , Math.round(pair[1]));
+            dataEditor.apply();
+            RateText.setText("");
+            WakeText.setText("");
+            SleepText.setText("");
+            saveAlert.show();
+        } else{
+            //wrong format
+            formatAlert.show();
+        }
+
+
+
+    }
+
+    public boolean checkFormat(View view)
+    {
+        //check if inputs are empty before proceeding
+        boolean notEmpty = (!WakeText.getText().toString().isEmpty()) && (!SleepText.getText().toString().isEmpty()) && (!RateText.getText().toString().isEmpty());
+
+        if (notEmpty){
+            //check 1-5 for rating, and 24 hr formatted times for sleep/wake
+            int Rating = Integer.parseInt(RateText.getText().toString());
+            boolean ratingFormat = (Rating >= 1 && Rating <= 5);
+
+            //check 24hr format
+            Pattern pattern;
+            Matcher matcher;
+            pattern = Pattern.compile("([01]?[0-9]|2[0-3]):[0-5][0-9]");
+            String WakeTime = WakeText.getText().toString();
+            String SleepTime = SleepText.getText().toString();
+
+            return ratingFormat && pattern.matcher(WakeTime).matches() && pattern.matcher(SleepTime).matches();
+        } else {
+            return false;
+        }
+
     }
 
 }
