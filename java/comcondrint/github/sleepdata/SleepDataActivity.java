3dd0a6337dcada5e32756551a3aa181c348ebc55
Trenton Condrin <condrint@gmail.com>
2017-07-29 19:13:50 -0700

    working on delete feature reupload

--- java/comcondrint/github/sleepdata/SleepDataActivity.java
+++ java/comcondrint/github/sleepdata/SleepDataActivity.java
@@ -1,9 +1,12 @@
 package comcondrint.github.sleepdata;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.support.v7.app.AlertDialog;
 
 import java.util.Map;
 
@@ -12,6 +15,8 @@ import static comcondrint.github.sleepdata.R.id.sleepdatadisplay;
 public class SleepDataActivity extends AppCompatActivity {
 
     public static TextView sleepdata;
+    EditText DeleteText;
+    AlertDialog formatAlert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +24,28 @@ public class SleepDataActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sleep_data);
         sleepdata = (TextView)findViewById(sleepdatadisplay);
         SleepDataActivity.sleepdata.setText(MainActivity.pref.getAll().toString());
+        DeleteText = (EditText)findViewById(R.id.DeleteIndex);
+
+        formatAlert = new AlertDialog.Builder(SleepDataActivity.this).create();
+        formatAlert.setMessage("Format must be an integer that exists in the data");
 
     }
 
-    public void refresh (View view)
+    public void delete (View view)
     {
-        SleepDataActivity.sleepdata.setText(MainActivity.pref.getAll().toString());
+        if (checkFormat(view))
+        {
+            Integer index = Integer.parseInt(DeleteText.getText().toString());
+
+        }
+        else
+        {
+            formatAlert.show();
+        }
+    }
+
+    public boolean checkFormat(View view)
+    {
+        return true;
     }
 }
